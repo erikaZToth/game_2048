128741f99c0b4fe1e6e2976052f847e90956a0f2
Erika Toth <erikatoth.0831@gmail.com>
2018-09-15 16:47:43 +0200

    Moving up numbers assigned to a separate function

--- Game_2048.py
+++ Game_2048.py
@@ -42,15 +42,47 @@ def jump_to_next_available_cell():
     pass
 
 
-def row_1_up(board, directions, board_size):
+def moving_up(board, directions, board_size):
+    for i in range(board_size):
+        for j in range(board_size):
+            # row_1_up(board, directions, board_size)
+            if board[0][j] == " ":
+                board[0][j] = board[1][j]
+                board[1][j] = board[2][j]
+                board[2][j] = board[3][j]
+                board[3][j] = " "
+            # row_2_up(board, directions, board_size)
+            if board[1][j] == " ":
+                board[1][j] = board[2][j]
+                board[2][j] = board[3][j]
+                board[3][j] = " "
+            # row_3_up(board, directions, board_size)
+            if board[2][j] == " ":
+                board[2][j] = board[3][j]
+                board[3][j] = " "
+            if i > 0:
+                if board[i][j] == board[i - 1][j] and board[i - 1][j] != " ": 
+                    board[i - 1][j] *= 2  # bug: if row0 == row2 AND row3 == row4, then row3 won't be doubled!!!
+                    board[i][j] = " "
+                    if i < (board_size - 1):
+                        board[i][j] = board[i + 1][j]
+                        board[i + 1][j] = " "
+                    else:
+                        board[i][j] = " "
+    # os.system("clear")
+    end_of_game(board, directions, board_size)  
+    return
+
+
+def moving_down(board, directions, board_size):
     pass
 
 
-def row_2_up(board, directions, board_size):
+def moving_left(board, directions, board_size):
     pass
 
 
-def row_3_up(board, directions, board_size):
+def moving_right(board, directions, board_size):
     pass
 
 
@@ -59,11 +91,11 @@ def end_of_game(board, directions, board_size):
     board_in_one_list = [0 if i == " " else i for i in board_in_one_list]
     maximum_value = max(board_in_one_list)
     # player reached 2048?
-    if maximum_value >= 64:
+    if maximum_value >= 2048:
         print_board(board_size, board)
-        print("Congratulations! You've reached 2048")  
+        print("Congratulations! You've reached 2048!!! :)")  
         exit()
-    # can we make steps?
+    # can we make any further steps?
     if 0 not in board_in_one_list:
         for i in range(board_size):
             for j in (1, board_size - 2):
@@ -83,34 +115,7 @@ def moving_on_board(board, directions, board_size):
     while True:
         direction = input("Enter direction: u (up), d (down), l (left), r (right): ")
         if direction == "u":
-            for i in range(board_size):
-                for j in range(board_size):
-                    # row_1_up(board, directions, board_size)
-                    if board[0][j] == " ":
-                        board[0][j] = board[1][j]
-                        board[1][j] = board[2][j]
-                        board[2][j] = board[3][j]
-                        board[3][j] = " "
-                    # row_2_up(board, directions, board_size)
-                    if board[1][j] == " ":
-                        board[1][j] = board[2][j]
-                        board[2][j] = board[3][j]
-                        board[3][j] = " "
-                    # row_3_up(board, directions, board_size)
-                    if board[2][j] == " ":
-                        board[2][j] = board[3][j]
-                        board[3][j] = " "
-                    if i > 0:
-                        if board[i][j] == board[i - 1][j] and board[i - 1][j] != " ": 
-                            board[i - 1][j] *= 2  # bug: if row0 == row2 AND row3 == row4, then row3 won't be doubled!!!
-                            board[i][j] = " "
-                            if i < (board_size - 1):
-                                board[i][j] = board[i + 1][j]
-                                board[i + 1][j] = " "
-                            else:
-                                board[i][j] = " "
-            # os.system("clear")
-            end_of_game(board, directions, board_size)                              
+            moving_up(board, directions, board_size)                            
         elif direction == "d":
             end_of_game(board, directions, board_size)
         elif direction == "l":
